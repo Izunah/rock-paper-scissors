5bc90b53d41523a4252ad2ea1a34aa8205a4c21c
Damon Verreault <39389306+Izunah@users.noreply.github.com>
2023-03-13 10:30:04 -0600

    Game loop added

--- main.js
+++ main.js
@@ -18,21 +18,54 @@ function playRound(computerSelection, playerSelection) {
     const playerSelectionLowerCase = playerSelection.toLowerCase();
 
     if(computerSelection == playerSelectionLowerCase)
-        return "You Draw!";
+        return ["You Draw!", 2];
     else if(playerSelectionLowerCase == "rock" && computerSelection == "paper")
-        return "You Lose! Paper beats Rock";
+        return ["You Lose! Paper beats Rock", 0];
     else if(playerSelectionLowerCase == "rock" && computerSelection == "scissors")
-        return "You Win! Rock beats Scissors";
+        return ["You Win! Rock beats Scissors", 1];
     else if(playerSelectionLowerCase == "paper" && computerSelection == "rock")
-        return "You Win! Paper beats Rock";
+        return ["You Win! Paper beats Rock", 1];
     else if(playerSelectionLowerCase == "paper" && computerSelection == "scissors")
-        return "You Lose! Scissors beats Paper";
+        return ["You Lose! Scissors beats Paper", 0];
     else if(playerSelectionLowerCase == "scissors" && computerSelection == "rock")
-        return "You Lose! Rock beats Scissors";
+        return ["You Lose! Rock beats Scissors", 0];
     else if(playerSelectionLowerCase == "scissors" && computerSelection == "paper")
-        return "You Win! Scissors beats Paper";
+        return ["You Win! Scissors beats Paper", 1];
     else
-        return "ERROR, INVALID COMBINATION";
+        return ["ERROR, INVALID COMBINATION", -1];
 }
 
-console.log(playRound(getComputerChoice(), "rock"));
+function game() {
+    let playerScore = 0;
+    let computerScore = 0;
+    
+    for (let i = 0; i < 5; i++)
+    {
+        const playerSelection = prompt("Pick one: Rock, Paper, Scissors");
+        let roundResult = playRound(getComputerChoice(), playerSelection);
+        console.log(roundResult[0]);
+
+        switch(roundResult[1]) {
+            case 0:
+                computerScore++;
+                break;
+            case 1:
+                playerScore++;
+                break;
+            case 2:
+                break;
+            default:
+                console.log("ERROR PROCESSING ROUND RESULT");
+                break;
+        }
+    }
+
+    if(playerScore > computerScore)
+        console.log(`Player Wins! ${playerScore} to ${computerScore}`);
+    else if(computerScore > playerScore)
+        console.log(`Computer Wins! ${computerScore} to ${playerScore}`);
+    else
+        console.log(`Game Draw! ${playerScore} to ${computerScore}`);
+}
+
+game();
